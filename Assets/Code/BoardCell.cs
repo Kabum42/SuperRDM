2232acd8633551cac9a32b62da9e87846b2d8fb3
Kabum42 <kabum42@gmail.com>
2015-09-16 13:28:26 +0200

    Board Procedural Generation (Rings)

--- Assets/Code/BoardCell.cs
+++ Assets/Code/BoardCell.cs
@@ -6,11 +6,28 @@ public class BoardCell : MonoBehaviour  {
     public string biome = "null";
     public GameObject root;
 
-    public BoardCell(string auxBiome)
+    public BoardCell()
+    {
+        
+        root = Instantiate(Resources.Load("Prefabs/BoardCell")) as GameObject;
+
+        float aux = Random.Range(0f, 1f);
+
+        if (aux < 1f/3f)
+        {
+            changeBiome("desert");
+        }
+        else if (aux < 2f/3f)
+        {
+            changeBiome("mountain");
+        }
+       
+    }
+
+    public void changeBiome(string auxBiome)
     {
         biome = auxBiome;
-        root = Instantiate(Resources.Load("Prefabs/BoardCell")) as GameObject;
-        root.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BoardCells/" +biome);
+        root.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BoardCells/" + biome);
     }
 
 }
--- Assets/Code/WorldScript.cs
+++ Assets/Code/WorldScript.cs
@@ -6,6 +6,9 @@ public class WorldScript : MonoBehaviour {
     public GameObject board;
     public BoardCell[] boardCells = new BoardCell[1];
 
+    private float cellWidth = 1.48f;
+    private float cellHeight = 1.51f;
+
 	// Use this for initialization
 	void Start () {
         // GlobalData.Start();
@@ -23,8 +26,115 @@ public class WorldScript : MonoBehaviour {
 
     void GenerateBoard()
     {
-        BoardCell b = new BoardCell("desert");
-        b.root.transform.parent = board.transform;
-        b.root.name = "Cell_1_1";
+        // RING 0
+        makeRing(0);
+
+        // RING 1
+        makeRing(1);
+
+        // RING 2
+        makeRing(2);
+
+        makeRing(3);
+
     }
+
+    private void makeRing(int number)
+    {
+        BoardCell b;
+        int currentCounter = 0;
+        float currentWidth = 0f;
+        float currentHeight = number;
+        
+
+        //INITIAL CELL
+        b = new BoardCell();
+        b.root.transform.parent = board.transform;
+        b.root.name = "Cell_" + number + "_" + currentCounter;
+        positionCell(b, currentWidth, currentHeight);
+        currentCounter++;
+
+        //DIAGONAL TO THE RIGHT COLUMN
+        while (currentWidth < number)
+        {
+            currentWidth += 1f;
+            currentHeight -= 0.5f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+        //RIGHT COLUMN
+        for (int i = 0; i < number; i++)
+        {
+            currentHeight -= 1f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+        // DIAGONAL TO THE CENTRAL DOWN
+        while (currentWidth > 0)
+        {
+            currentWidth -= 1f;
+            currentHeight -= 0.5f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+        // DIAGONAL TO THE LEFT COLUMN
+        while (currentWidth > -number)
+        {
+            currentWidth -= 1f;
+            currentHeight += 0.5f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+        //LEFT COLUMN
+        for (int i = 0; i < number; i++)
+        {
+            currentHeight += 1f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+        // DIAGONAL TO THE INITIAL
+        while (currentWidth < -1)
+        {
+            currentWidth += 1f;
+            currentHeight += 0.5f;
+
+            b = new BoardCell();
+            b.root.transform.parent = board.transform;
+            b.root.name = "Cell_" + number + "_" + currentCounter;
+            positionCell(b, currentWidth, currentHeight);
+            currentCounter++;
+        }
+
+    }
+
+    private void positionCell(BoardCell b, float width, float height)
+    {
+        b.root.transform.position = board.transform.position + new Vector3(width * cellWidth, 0.1f, height * cellHeight);
+    }
+
 }
