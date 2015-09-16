dca4af5a22d08834d5c124c13a3f555883de8e8a
Kabum42 <kabum42@gmail.com>
2015-09-16 02:51:13 +0200

    Basic Test

---
+++ Assets/Code/BoardCell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardCell : MonoBehaviour  {
+
+    public string biome = "null";
+    public GameObject root;
+
+    public BoardCell(string auxBiome)
+    {
+        biome = auxBiome;
+        root = Instantiate(Resources.Load("Prefabs/BoardCell")) as GameObject;
+        root.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("BoardCells/" +biome);
+    }
+
+}
---
+++ Assets/Code/WorldScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldScript : MonoBehaviour {
+
+    public GameObject board;
+    public BoardCell[] boardCells = new BoardCell[1];
+
+	// Use this for initialization
+	void Start () {
+        // GlobalData.Start();
+        board = new GameObject();
+        board.name = "Board";
+        board.transform.position = new Vector3(0f, 0f, 0f);
+
+        GenerateBoard();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void GenerateBoard()
+    {
+        BoardCell b = new BoardCell("desert");
+        b.root.transform.parent = board.transform;
+        b.root.name = "Cell_1_1";
+    }
+}
