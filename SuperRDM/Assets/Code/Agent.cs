58573a469cdbca3c81ff698840d139f7c2d24f94
Kabum42 <kabum42@gmail.com>
2015-09-16 02:01:17 +0200

    Initial Status

---
+++ SuperRDM/Assets/Code/Agent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent {
+
+	// Use this for initialization
+	
+    public Agent()
+    {
+        Debug.Log("Agent created");
+    }
+
+}
---
+++ SuperRDM/Assets/Code/GlobalData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GlobalData {
+
+    public static bool started = false;
+    public static Agent[] agents = new Agent[6];
+
+	// Use this for initialization
+	public static void Start () {
+        started = true;
+        agents[0] = new Agent();
+	}
+	
+
+}
