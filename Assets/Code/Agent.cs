4402cfb308d155d4baaae622cb5bdcb30536cbb0
Kabum42 <kabum42@gmail.com>
2015-09-17 14:09:24 +0200

    Added Placeholder Music

--- Assets/Code/Agent.cs
+++ Assets/Code/Agent.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class Agent {
 
-	// Use this for initialization
+    // Use this for initialization
+    public string champion = "Barbarian";
+    public int level = 1;
+    public double experience = 0f;
+    public double gold = 0f;
 	
     public Agent()
     {
