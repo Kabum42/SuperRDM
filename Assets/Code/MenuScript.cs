cb8dd24aca68112eb8039dfc8cbf555d13f963b5
Kabum42 <kabum42@gmail.com>
2015-09-19 02:27:00 +0200

    Deleted Preparation Scene, fused with Menu

--- Assets/Code/MenuScript.cs
+++ Assets/Code/MenuScript.cs
@@ -13,6 +13,8 @@ public class MenuScript : MonoBehaviour {
     public GameObject button2;
     public AudioSource intro;
     private float transition = 1f;
+    private float phase = 0f;
+    private GameObject agent1;
 
     // Use this for initialization
     void Start () {
@@ -27,113 +29,144 @@ public class MenuScript : MonoBehaviour {
         logoPosition = logo.transform.position;
         logo.GetComponent<SpriteRenderer>().color = new Color(logo.GetComponent<SpriteRenderer>().color.r, logo.GetComponent<SpriteRenderer>().color.g, logo.GetComponent<SpriteRenderer>().color.b, 0f);
 
+        agent1 = GameObject.Find("Agent1");
+        agent1.SetActive(false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (transition < 1f)
+        if (phase == 0f)
         {
-            transition -= Time.deltaTime;
-            if (transition < 0f)
-            {
-                Application.LoadLevel("Preparation");
-            }
-
-            logo.GetComponent<SpriteRenderer>().color = new Color(logo.GetComponent<SpriteRenderer>().color.r, logo.GetComponent<SpriteRenderer>().color.g, logo.GetComponent<SpriteRenderer>().color.b, transition);
-
-            startText.GetComponent<TextMesh>().color = new Color(startText.GetComponent<TextMesh>().color.r, startText.GetComponent<TextMesh>().color.g, startText.GetComponent<TextMesh>().color.b, transition);
-            startText2.GetComponent<TextMesh>().color = new Color(startText2.GetComponent<TextMesh>().color.r, startText2.GetComponent<TextMesh>().color.g, startText2.GetComponent<TextMesh>().color.b, transition);
-
-            button1.GetComponent<SpriteRenderer>().color = new Color(button1.GetComponent<SpriteRenderer>().color.r, button1.GetComponent<SpriteRenderer>().color.g, button1.GetComponent<SpriteRenderer>().color.b, transition);
-            button2.GetComponent<SpriteRenderer>().color = new Color(button2.GetComponent<SpriteRenderer>().color.r, button2.GetComponent<SpriteRenderer>().color.g, button2.GetComponent<SpriteRenderer>().color.b, transition);
-
-            intro.volume = transition;
-
-        }
-
-        else
-        {
-
-            if (logo.GetComponent<SpriteRenderer>().color.a < 0.99f)
-            {
-                logo.GetComponent<SpriteRenderer>().color = new Color(logo.GetComponent<SpriteRenderer>().color.r, logo.GetComponent<SpriteRenderer>().color.g, logo.GetComponent<SpriteRenderer>().color.b, Mathf.Lerp(logo.GetComponent<SpriteRenderer>().color.a, 1f, Time.deltaTime));
-            }
-
-            radius2 += Time.deltaTime * 100f;
-            if (radius2 > 360f) { radius2 -= 360f; }
-
-            startText.GetComponent<TextMesh>().color = new Color(startText.GetComponent<TextMesh>().color.r, startText.GetComponent<TextMesh>().color.g, startText.GetComponent<TextMesh>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
-            startText2.GetComponent<TextMesh>().color = new Color(startText2.GetComponent<TextMesh>().color.r, startText2.GetComponent<TextMesh>().color.g, startText2.GetComponent<TextMesh>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
-
-            button1.GetComponent<SpriteRenderer>().color = new Color(button1.GetComponent<SpriteRenderer>().color.r, button1.GetComponent<SpriteRenderer>().color.g, button1.GetComponent<SpriteRenderer>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
-            button2.GetComponent<SpriteRenderer>().color = new Color(button2.GetComponent<SpriteRenderer>().color.r, button2.GetComponent<SpriteRenderer>().color.g, button2.GetComponent<SpriteRenderer>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
-
-
-        }
-
-        radius += Time.deltaTime*100f;
-        if (radius > 360f) { radius -= 360f; }
-
-        logo.transform.localPosition = logoPosition + new Vector3(0f, Mathf.Sin(radius * Mathf.PI / 180)*0.25f, 0f);
-
-        
-        int controllerConnected = -1;
-        for (int i = 0; i < Input.GetJoystickNames().Length; i++)
-        {
-            if (Input.GetJoystickNames()[i] != "")
-            {
-                controllerConnected = i;
-                break;
-            }
-        }
-
-        if (controllerConnected != -1)
-        {
-            // CONTROLLER PLUGGED
-            if (button1.activeInHierarchy) { button1.SetActive(false); }
-            if (!button2.activeInHierarchy) { button2.SetActive(true); }
-
-            if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsWebPlayer)
-            {
-                // WINDOWS
-                if (Input.GetKeyDown("joystick button 0"))
-                {
-                    transition -= Time.deltaTime;
-                }
-            }
-            if (Application.platform == RuntimePlatform.OSXDashboardPlayer || Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXWebPlayer)
-            {
-                // MAC
-                if (Input.GetKeyDown("joystick button 16"))
-                {
-                    transition -= Time.deltaTime;
-                }
-            }
-            if (Application.platform == RuntimePlatform.LinuxPlayer)
-            {
-                // LINUX
-                if (Input.GetKeyDown("joystick button 0"))
-                {
-                    transition -= Time.deltaTime;
-                }
-            }
-
-        }
-        else
-        {
-            // CONTROLLER NOT PLUGGED
-            if (button2.activeInHierarchy) { button2.SetActive(false); }
-            if (!button1.activeInHierarchy) { button1.SetActive(true); }
-
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (transition < 1f)
             {
                 transition -= Time.deltaTime;
+
+                logo.GetComponent<SpriteRenderer>().color = new Color(logo.GetComponent<SpriteRenderer>().color.r, logo.GetComponent<SpriteRenderer>().color.g, logo.GetComponent<SpriteRenderer>().color.b, transition);
+
+                startText.GetComponent<TextMesh>().color = new Color(startText.GetComponent<TextMesh>().color.r, startText.GetComponent<TextMesh>().color.g, startText.GetComponent<TextMesh>().color.b, transition);
+                startText2.GetComponent<TextMesh>().color = new Color(startText2.GetComponent<TextMesh>().color.r, startText2.GetComponent<TextMesh>().color.g, startText2.GetComponent<TextMesh>().color.b, transition);
+
+                button1.GetComponent<SpriteRenderer>().color = new Color(button1.GetComponent<SpriteRenderer>().color.r, button1.GetComponent<SpriteRenderer>().color.g, button1.GetComponent<SpriteRenderer>().color.b, transition);
+                button2.GetComponent<SpriteRenderer>().color = new Color(button2.GetComponent<SpriteRenderer>().color.r, button2.GetComponent<SpriteRenderer>().color.g, button2.GetComponent<SpriteRenderer>().color.b, transition);
+
+                intro.volume = transition;
+
+            }
+
+            else
+            {
+
+                if (logo.GetComponent<SpriteRenderer>().color.a < 0.99f)
+                {
+                    logo.GetComponent<SpriteRenderer>().color = new Color(logo.GetComponent<SpriteRenderer>().color.r, logo.GetComponent<SpriteRenderer>().color.g, logo.GetComponent<SpriteRenderer>().color.b, Mathf.Lerp(logo.GetComponent<SpriteRenderer>().color.a, 1f, Time.deltaTime));
+                }
+
+                radius2 += Time.deltaTime * 100f;
+                if (radius2 > 360f) { radius2 -= 360f; }
+
+                startText.GetComponent<TextMesh>().color = new Color(startText.GetComponent<TextMesh>().color.r, startText.GetComponent<TextMesh>().color.g, startText.GetComponent<TextMesh>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
+                startText2.GetComponent<TextMesh>().color = new Color(startText2.GetComponent<TextMesh>().color.r, startText2.GetComponent<TextMesh>().color.g, startText2.GetComponent<TextMesh>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
+
+                button1.GetComponent<SpriteRenderer>().color = new Color(button1.GetComponent<SpriteRenderer>().color.r, button1.GetComponent<SpriteRenderer>().color.g, button1.GetComponent<SpriteRenderer>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
+                button2.GetComponent<SpriteRenderer>().color = new Color(button2.GetComponent<SpriteRenderer>().color.r, button2.GetComponent<SpriteRenderer>().color.g, button2.GetComponent<SpriteRenderer>().color.b, Mathf.Abs(Mathf.Sin(radius2 * Mathf.PI / 180)));
+
+
+            }
+
+            radius += Time.deltaTime * 100f;
+            if (radius > 360f) { radius -= 360f; }
+
+            logo.transform.localPosition = logoPosition + new Vector3(0f, Mathf.Sin(radius * Mathf.PI / 180) * 0.25f, 0f);
+
+
+            int controllerConnected = -1;
+            for (int i = 0; i < Input.GetJoystickNames().Length; i++)
+            {
+                if (Input.GetJoystickNames()[i] != "")
+                {
+                    controllerConnected = i;
+                    break;
+                }
+            }
+
+            if (controllerConnected != -1)
+            {
+                // CONTROLLER PLUGGED
+                if (button1.activeInHierarchy) { button1.SetActive(false); }
+                if (!button2.activeInHierarchy) { button2.SetActive(true); }
+
+                if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsWebPlayer)
+                {
+                    // WINDOWS
+                    if (Input.GetKeyDown("joystick button 0"))
+                    {
+                        transition -= Time.deltaTime;
+                    }
+                }
+                if (Application.platform == RuntimePlatform.OSXDashboardPlayer || Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXWebPlayer)
+                {
+                    // MAC
+                    if (Input.GetKeyDown("joystick button 16"))
+                    {
+                        transition -= Time.deltaTime;
+                    }
+                }
+                if (Application.platform == RuntimePlatform.LinuxPlayer)
+                {
+                    // LINUX
+                    if (Input.GetKeyDown("joystick button 0"))
+                    {
+                        transition -= Time.deltaTime;
+                    }
+                }
+            }
+            else
+            {
+                // CONTROLLER NOT PLUGGED
+                if (button2.activeInHierarchy) { button2.SetActive(false); }
+                if (!button1.activeInHierarchy) { button1.SetActive(true); }
+
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    transition -= Time.deltaTime;
+                }
+
+            }
+
+
+            if (transition < 0f)
+            {
+                phase = 1f;
+                logo.SetActive(false);
+                startText.SetActive(false);
+                startText2.SetActive(false);
+                button1.SetActive(false);
+                button2.SetActive(false);
+                transition = 0f;
+
+                agent1.SetActive(true);
+                agent1.GetComponent<SpriteRenderer>().color = new Color(agent1.GetComponent<SpriteRenderer>().color.r, agent1.GetComponent<SpriteRenderer>().color.g, agent1.GetComponent<SpriteRenderer>().color.b, 0f);
             }
 
         }
+        else if (phase == 1f)
+        {
+            if (transition < 1f)
+            {
+                transition += Time.deltaTime;
+                if (transition > 1f)
+                {
+                    transition = 1f;
+                }
+                agent1.GetComponent<SpriteRenderer>().color = new Color(agent1.GetComponent<SpriteRenderer>().color.r, agent1.GetComponent<SpriteRenderer>().color.g, agent1.GetComponent<SpriteRenderer>().color.b, transition);
+                intro.volume = transition;
+            }
+        }
 
-        
+
+
 
     }
 }
