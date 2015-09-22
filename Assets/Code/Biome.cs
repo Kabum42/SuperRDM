8671b32d8840c3d1408b28c5de4320d6db4d6027
FrVonLettow <MCasquero3@gmail.com>
2015-09-23 01:27:56 +0200

    Iniciando Clases

---
+++ Assets/Code/Biome.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Biome {
+	public int Grassland = 1;
+	public int Forest = 2;
+	public int Swamp = 3;
+	public int Lake = 4;
+	public int Dessert = 5;
+	public int Mountain = 6;
+	
+}
---
+++ Assets/Code/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character {
+	
+	// private Skill[] Skills = new Skill[3];
+	private string Name;
+	private float CurrentHealth;
+	private float MaxHealth;
+	private float SpeedTurnBar;
+	private float ProgressTurnBar;
+
+	public Character()
+	{
+		Debug.Log("Character created");
+	}
+	
+}
