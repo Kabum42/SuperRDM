f547ca3460866396e9815f6dd798c86716cdd5b3
Kabum42 <kabum42@gmail.com>
2015-09-23 11:48:40 +0200

    Merge correction

--- Assets/Code/GlobalData.cs
+++ Assets/Code/GlobalData.cs
@@ -5,13 +5,13 @@ public static class GlobalData {
 
     public static bool started = false;
     public static string OS;
-    public static Agent[] agents = new Agent[6];
+    public static MainCharacter[] agents = new MainCharacter[6];
 
 	// Use this for initialization
 	public static void Start () {
 
         started = true;
-        agents[0] = new Agent();
+        agents[0] = new MainCharacter();
 
         if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsWebPlayer)
         {
