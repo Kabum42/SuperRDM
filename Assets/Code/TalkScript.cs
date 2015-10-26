23c7e884ab08320e36018f1afc2289c52e0cfb76
Kabum42 <kabum42@gmail.com>
2015-10-26 23:02:58 +0100

    Beautiful conversation

--- Assets/Code/TalkScript.cs
+++ Assets/Code/TalkScript.cs
@@ -108,13 +108,23 @@ public class TalkScript : MonoBehaviour {
         s.text.transform.position = new Vector3(b.position.x - s.text.GetComponent<Renderer>().bounds.size.x / 2f,b.position.y + s.text.GetComponent<Renderer>().bounds.size.y / 2f, s.text.transform.position.z);
         float scaleX = s.text.GetComponent<Renderer>().bounds.size.x;
         float scaleY = s.text.GetComponent<Renderer>().bounds.size.y;
-        if (scaleX < scaleY * 0.5f + 0.5f) { scaleX = scaleY * 0.5f + 0.5f; }
-        if (scaleY < scaleX * 0.25f +0.2f) { scaleY = scaleX * 0.25f +0.2f; }
-        s.bubble.transform.localScale = new Vector3(Mathf.Lerp(s.bubble.transform.localScale.x, scaleX * 0.7f, Time.deltaTime * 20f), Mathf.Lerp(s.bubble.transform.localScale.y, scaleY * 0.7f, Time.deltaTime * 20f), 1f);
+		if (scaleX > scaleY) { scaleY += 0.5f; }
+		if (scaleY > scaleX) { scaleX += 0.5f; }
+        //if (scaleX < scaleY * 0.5f) { scaleX = scaleY * 0.5f; }
+        //if (scaleY < scaleX * 0.25f) { scaleY = scaleX * 0.25f; }
+        s.bubble.transform.localScale = new Vector3(Mathf.Lerp(s.bubble.transform.localScale.x, (scaleX) * 0.7f, Time.deltaTime * 20f), Mathf.Lerp(s.bubble.transform.localScale.y, (scaleY) * 0.7f, Time.deltaTime * 20f), 1f);
         s.bubble.transform.position = new Vector3(s.text.GetComponent<Renderer>().bounds.center.x, s.text.GetComponent<Renderer>().bounds.center.y, s.bubble.transform.position.z);
         s.bubble2.transform.localScale = new Vector3(s.bubble.transform.localScale.x + 0.035f, s.bubble.transform.localScale.y + 0.035f, s.bubble2.transform.localScale.z);
         s.bubble2.transform.position = s.bubble.transform.position;
-        s.text.GetComponent<TextMesh>().text = currentString;
+      	
+		float min = s.bubble.transform.localScale.x;
+		if (s.bubble.transform.localScale.y < min) { min = s.bubble.transform.localScale.y; }
+		s.triangle.transform.localScale = new Vector3(min/3f, min/3f, min/3f);
+		s.triangle.transform.position = new Vector3 (s.bubble.GetComponent<Renderer>().bounds.center.x, s.bubble.GetComponent<Renderer>().bounds.center.y - s.triangle.GetComponent<Renderer>().bounds.size.y/2f -s.bubble.GetComponent<Renderer>().bounds.size.y/2f +0.01f, s.triangle.transform.position.z);
+		s.triangle2.transform.localScale = new Vector3 (s.triangle.transform.localScale.x + 0.035f, s.triangle.transform.localScale.y + 0.035f, s.triangle2.transform.localScale.z);
+		s.triangle2.transform.position = new Vector3 (s.triangle.transform.position.x, s.triangle.transform.position.y -0.04f, s.triangle2.transform.position.z);
+
+		s.text.GetComponent<TextMesh>().text = currentString;
 
         if (Input.GetKeyDown(KeyCode.Return) && s.currentLetter > 0)
         {
@@ -195,6 +205,8 @@ public class TalkScript : MonoBehaviour {
         public GameObject text;
         public GameObject bubble;
         public GameObject bubble2;
+		public GameObject triangle;
+		public GameObject triangle2;
         public int currentLetter = 0;
         public float timer = 0f;
 
@@ -204,6 +216,8 @@ public class TalkScript : MonoBehaviour {
             text = root.transform.FindChild("Text").gameObject;
             bubble = root.transform.FindChild("Bubble").gameObject;
             bubble2 = root.transform.FindChild("Bubble2").gameObject;
+			triangle = root.transform.FindChild("Triangle").gameObject;
+			triangle2 = root.transform.FindChild("Triangle2").gameObject;
         }
 
     }
