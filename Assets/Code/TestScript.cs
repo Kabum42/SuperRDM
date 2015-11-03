7bfcc0f04b969ab69c1a6ed26a30dd4297528bf1
Ruben Tarin <kabum42@gmail.com>
2015-11-03 22:25:52 +0100

    Visual Characters

--- Assets/Code/TestScript.cs
+++ Assets/Code/TestScript.cs
@@ -3,17 +3,37 @@ using System.Collections;
 
 public class TestScript : MonoBehaviour {
 
+    
+
+    private VisualCharacter player1;
+    private VisualCharacter player2;
+
 	// Use this for initialization
 	void Start () {
-        //SkillBar.root = Instantiate(Resources.Load("Prefabs/SkillBarObject")) as GameObject;
+        /*
         SkillBar s = (Instantiate(Resources.Load("Prefabs/SkillBarObject")) as GameObject).GetComponent<SkillBar>();
         s.setBad(-0.95f, 0.65f);
         s.setGood(-0.85f, 0.15f);
         s.setCritical(-0.75f, -0.5f);
+         * */
+
+        GlobalData.Start();
+
+        player1 = (Instantiate(Resources.Load("Prefabs/VisualCharacterObject")) as GameObject).GetComponent<VisualCharacter>();
+        player2 = (Instantiate(Resources.Load("Prefabs/VisualCharacterObject")) as GameObject).GetComponent<VisualCharacter>();
+        player1.SetBattlePosition(true, 0);
+        player2.SetBattlePosition(false, 0);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            player1.Perform(GlobalData.Skills[2], player2);
+            //henmancer.Represent(GlobalData.Skills[2]);
+        }
 	
 	}
 }
---
+++ Assets/Code/VisualCharacter.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualCharacter : MonoBehaviour {
+
+    public GameObject root;
+    public int performing = -1;
+    private VisualCharacter targetPerformance = null;
+    private bool side = true;
+    private GameObject character;
+    private GameObject bloodBarbarian;
+    private AudioSource axeHit;
+
+    private string statusPerformance = "none";
+
+	// Use this for initialization
+	void Awake () {
+        root = this.gameObject;
+        character = root.transform.FindChild("Character").gameObject;
+        bloodBarbarian = root.transform.FindChild("Character/BloodBarbarian").gameObject;
+        bloodBarbarian.SetActive(false);
+
+        axeHit = gameObject.AddComponent<AudioSource>();
+        axeHit.clip = Resources.Load("Music/Hits/AxeHit") as AudioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (statusPerformance == "chasing")
+        {
+            // ACERCARSE
+            if (side)
+            {
+                if ((root.transform.position.x + character.GetComponent<SpriteRenderer>().bounds.size.x / 2f) < targetPerformance.root.transform.position.x)
+                {
+                    root.transform.position = new Vector3(Mathf.Lerp(root.transform.position.x, targetPerformance.root.transform.position.x, Time.deltaTime * 10f), root.transform.position.y, root.transform.position.z);
+
+                }
+                else
+                {
+                    statusPerformance = "executing";
+                }
+            }
+
+        }
+        else if (statusPerformance == "returning")
+        {
+            // VUELVE A SU SITIO
+            if (side)
+            {
+                if (root.transform.position.x > -4.95f)
+                {
+                    root.transform.position = new Vector3(Mathf.Lerp(root.transform.position.x, -5f, Time.deltaTime * 10f), root.transform.position.y, root.transform.position.z);
+                }
+                else
+                {
+                    statusPerformance = "none";
+
+                    float scaleX = root.transform.localScale.x;
+                    if (side)
+                    {
+                        scaleX = Mathf.Abs(scaleX);
+                    }
+                    else
+                    {
+                        scaleX = -Mathf.Abs(scaleX);
+                    }
+                    root.transform.localScale = new Vector3(scaleX, root.transform.localScale.y, root.transform.localScale.z);
+                }
+            }
+        }
+        else if (statusPerformance == "executing")
+        {
+            // HACE EL ATAQUE
+            if (performing == 2)
+            {
+                // ES EL FINISHER DEL BARBARO
+                targetPerformance.Represent(GlobalData.Skills[performing]);
+                performing = -1;
+                targetPerformance = null;
+                statusPerformance = "returning";
+
+                float scaleX = root.transform.localScale.x;
+                if (side)
+                {
+                    scaleX = -Mathf.Abs(scaleX);
+                }
+                else
+                {
+                    scaleX = Mathf.Abs(scaleX);
+                }
+                root.transform.localScale = new Vector3(scaleX, root.transform.localScale.y, root.transform.localScale.z);
+            }
+            
+        }
+
+        
+
+
+
+        if (bloodBarbarian.activeInHierarchy)
+        {
+            if (bloodBarbarian.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                bloodBarbarian.SetActive(false);
+            }
+        }
+	
+	}
+
+    public void SetBattlePosition(bool auxSide, int number)
+    {
+        if (auxSide)
+        {
+            root.transform.position = new Vector3(-5f, root.transform.position.y, root.transform.position.z);
+        }
+        else
+        {
+            root.transform.localScale = new Vector3(-root.transform.localScale.x, root.transform.localScale.y, root.transform.localScale.z);
+            root.transform.position = new Vector3(5f, root.transform.position.y, root.transform.position.z);
+        }
+        side = auxSide;
+    }
+
+    public void Represent(Skill s)
+    {
+        if (s.getID() == 2)
+        {
+            // ES EL FINISHER DEL BARBARO
+            axeHit.Play();
+            bloodBarbarian.SetActive(true);
+            Hacks.SpriteRendererAlpha(bloodBarbarian, 1f);
+        }
+    }
+
+    public void Represent(Skill s, float stack)
+    {
+
+    }
+
+
+    public void Perform(Skill s, VisualCharacter v)
+    {
+
+        performing = s.getID();
+        targetPerformance = v;
+
+        if (performing == 2)
+        {
+            statusPerformance = "chasing";
+        }
+        
+    }
+
+    public void Perform(Skill s, VisualCharacter v, float stack)
+    {
+
+    }
+}
